7d72b24b547d253b8fbecbe0f897ebb31dbb142c
marie <ingabiremarieaimee6@gmail.com>
2023-12-11 10:14:02 -0800

    start push

---
+++ React-first/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import'../public/me1.jpg'
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1>Hello</h1>
+      <p>World!</p>
+      <button>aimee</button>
+      <img src="me1.jpg" alt="" />
+      </div>
+    
+    </>
+  )
+}
+
+export default App
